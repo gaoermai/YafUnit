48b0cc55339734d44e31321c1a559d4da116ed73
GaoErMai(808112) <gaoermai@91.com>
2014-04-14 13:45:05 +0800

    在YafUnit_TestCase中新增YafUnit_Request
    YafUnit_Request用来模拟浏览器请求，但没有调用到Route部分功能，所以目前还有一定局限性。

--- tests/YafUnit_TestCase.php
+++ tests/YafUnit_TestCase.php
@@ -2,6 +2,97 @@
 
 define('IN_YafUnit', true);
 
+
+final class YafUnit_Request extends Yaf\Request_Abstract
+{
+    public function __construct($method, $module, $controller, $action, $params = null)
+    {
+        $this->method     = $method;
+        $this->module     = $module;
+        $this->controller = $controller;
+        $this->action     = $action;
+        $this->params     = $params;
+        $this->__setParams();
+    }
+
+    public function getQuery($name = null)
+    {
+        if (is_null($name)) return $_GET;
+        return isset($_GET[$name]) ? $_GET[$name] : null;
+    }
+
+    public function getPost($name = null)
+    {
+        if (is_null($name)) return $_POST;
+        return isset($_POST[$name]) ? $_POST[$name] : null;
+    }
+
+    public function getCookie($name = null)
+    {
+        if ( is_null($name) ) return $_COOKIE;
+        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : null;
+    }
+
+    public function getFiles($name = null)
+    {
+        if (is_null($name)) return $_FILES;
+        return isset($_FILES[$name]) ? $_FILES[$name] : null;
+    }
+
+    public function getServer($name, $default = null)
+    {
+
+    }
+
+    public function getEnv($name, $default = null)
+    {
+
+    }
+
+    public function setPost($name, $value)
+    {
+        $_POST[$name] = $value;
+    }
+
+    public function setQuery($name, $value)
+    {
+        $_GET[$name] = $value;
+    }
+
+    public function setCookie($name, $value)
+    {
+        $_COOKIE[$name] = $value;
+    }
+
+    public function setSession($name, $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    public function getModuleName()
+    {
+        return $this->module;
+    }
+
+    public function getControllerName()
+    {
+        return $this->controller;
+    }
+
+    private function __setParams()
+    {
+        if (!empty ($this->params) && is_array($this->params)) {
+            foreach ($this->params as $key => $value) {
+                if ($this->method == 'POST') {
+                    $this->setPost($key, $value);
+                } else {
+                    $this->setQuery($key, $value);
+                }
+            }
+        }
+    }
+}
+
 final class YafUnit_View implements Yaf\View_Interface
 {
 
@@ -72,13 +163,15 @@ final class YafUnit_View implements Yaf\View_Interface
 
 class YafUnit_TestCase extends PHPUnit_Framework_TestCase
 {
-    protected static $_app;
+    protected $_app;
     protected static $_view;
 
     public function setUp()
     {
-        if (!self::$_app) {
+        if (!Yaf\Registry::get('Application')) {
             $this->__setUpYafApplication();
+        }else {
+            $this->_app = Yaf\Registry::get('Application');
         }
         if (!self::$_view) {
             self::$_view = YafUnit_View::getInstance();
@@ -95,9 +188,9 @@ class YafUnit_TestCase extends PHPUnit_Framework_TestCase
         $this->__setUpPHPIniVariables();
 
         require_once __DIR__.'/../public/index.php';
-        self::$_app = $app;
-        self::$_app->getDispatcher()->setView(YafUnit_View::getInstance());
-
-        Yaf\Registry::set('Application', self::$_app);
+        $this->_app = & $app;
+        $this->_app->bootstrap();
+        $this->_app->getDispatcher()->setView(YafUnit_View::getInstance());
+        Yaf\Registry::set('Application', $this->_app);
     }
 }
